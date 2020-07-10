245ad7cbffbd0f48f5e1d63eeaa8c22f719ce9af
Berlian Gigih Prakoso <berliangigihprakoso@gmail.com>
2020-07-10 10:34:39 +0700

    video 3 templating

--- b_Django/urls.py
+++ b_Django/urls.py
@@ -8,24 +8,17 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns: ppath('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Home Page')
-
-def contact(request):
-    return HttpResponse('Contact Page')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contact/', contact),
+    path('', include('accounts.url')),
 ]
